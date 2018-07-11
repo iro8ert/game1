1cba873f9b01d7bc6a34259f0ca9e996bfbf7790
Robert Nowaczyk <iro8ert@o2.pl>
2018-07-11 17:01:15 +0200

    add movement and bounce

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var x = 0;
+var y = 0;
+var dx = 1;
+var dy = 1;
+var ballRadius = 10;
 
+function drawBall() {
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx;
+	y += dy;
+	if (y + dy < 0 || y + dy > canvas.height ){
+		dy = -dy;
+	}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 25, 0,5)";
-ctx.stroke();
-ctx.closePath();
+	if (x + dx < 0 || x + dx > canvas.width) {
+		dx = -dx;
+	}
+}
+setInterval(draw, 10);
